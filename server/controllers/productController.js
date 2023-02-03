40bd8e6e7d479420f7fbe16346ca137d24da907b
chinhnt113 <nguyenthechinhk25@gmail.com>
2023-02-03 08:58:51 +0700

    delete, get by id api

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -22,10 +22,24 @@ exports.updateProduct = async (req, res, next) => {
     }
 }
 exports.deleteProduct = async (req, res, next) => {
-    
+    const { id } = req.params
+    try {
+        const product = await Product.findByIdAndDelete(id)
+        if (!product) return res.status(404).json({ msg: "Product not found" })
+        return res.status(200).json(product)
+    } catch (error) {
+        return res.status(500).json({ msg: error.message || 'Something went wrong' })
+    }
 }
 exports.getProduct = async (req, res, next) => {
-    
+    const { id } = req.params
+    try {
+        const product = await Product.findById(id)
+        if (!product) return res.status(404).json({ msg: "Product not found" })
+        return res.status(200).json(product)
+    } catch (error) {
+        return res.status(500).json({ msg: error.message || 'Something went wrong' })
+    }
 }
 
 exports.getProducts = async (req, res, next) => {
