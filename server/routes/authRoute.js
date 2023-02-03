3e897e024bc94f78fe2178d143e0e973e15bc742
Phan Khánh <phankhanh658@gmail.com>
2023-02-03 23:02:07 +0700

    fix conflict

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -3,4 +3,4 @@ const { login, register } = require('../controllers/authController')
 router.post('/login', login)
 router.post('/register', register)
 
-module.exports = route
+module.exports = router
